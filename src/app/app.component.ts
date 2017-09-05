420330f67b86ad9d1590031158e576b65ce9b233
sivakumargsk <sivakumar.gsk@technoidentity.com>
2017-09-05 15:27:13 +0530

    Updated AppComponent as parent component for CounterComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  myCount = 10;
+  countChange(value) {
+    this.myCount = value;
+  }
 }
