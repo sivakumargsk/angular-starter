39fa8bc3876bf57d9902acca235fe2ef19395edc
sivakumargsk <sivakumar.gsk@technoidentity.com>
2017-09-06 15:48:11 +0530

    reseting master branch to initial project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  myCount = 10;
-  countChange(value) {
-    this.myCount = value;
-  }
+  title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CounterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
