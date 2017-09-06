67878bfd6d92bf746c6abb56b1004fbae172cf2c
sivakumargsk <sivakumar.gsk@technoidentity.com>
2017-09-06 14:28:07 +0530

    Added onInputchange method and updated change event emitter with counterChange

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -8,15 +8,26 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class CounterComponent {
   @Input() count = 0;
 
-  @Output() change: EventEmitter<number> = new EventEmitter();
+  @Output() counterChange: EventEmitter<number> = new EventEmitter();
+
+  onInputChange(value: string) {
+    const newValue = parseInt(value, 10);
+    if (newValue) {
+      this.count = newValue;
+      this.counterChange.emit(this.count);
+    } else {
+      this.count = 0;
+      this.counterChange.emit(0);
+    }
+  }
 
   increment() {
     this.count += 1;
-    this.change.emit(this.count);
+    this.counterChange.emit(this.count);
   }
 
   decrement() {
     this.count -= 1;
-    this.change.emit(this.count);
+    this.counterChange.emit(this.count);
   }
 }
