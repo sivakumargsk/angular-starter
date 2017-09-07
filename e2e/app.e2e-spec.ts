ff375528c962abb6713564029e350a1ae844fa73
sivakumargsk <sivakumar.gsk@technoidentity.com>
2017-09-07 17:16:32 +0530

    Updated dependency version and config's

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,10 +1,10 @@
-import { AngularStarterPage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('angular-starter App', () => {
-  let page: AngularStarterPage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new AngularStarterPage();
+    page = new AppPage();
   });
 
   it('should display welcome message', () => {
