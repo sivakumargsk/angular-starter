66d2b76d6e7c2dff7b9eeb75deba68ba7e2942f8
sivakumargsk <geddasiva777@gmail.com>
2017-09-25 19:17:14 +0530

    added todos components & formsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { TodoRowComponent } from './components/todo-row/todo-row.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoFotterComponent } from './components/todo-fotter/todo-fotter.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { TodosComponent } from './components/todos/todos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTodoComponent,
+    TodoRowComponent,
+    TodoListComponent,
+    TodoFotterComponent,
+    PaginationComponent,
+    TodosComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
